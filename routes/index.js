69f2fbdd99141be9fafba78d814fa27145df788a
D34D10CK <leonard.berney@epfl.ch>
2015-10-02 15:32:17 +0200

    start using mongo in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
+mongoose.connect('monodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (callback) => {
+//TODO
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
+
 	res.render('index', { title: 'Pollock' });
 });
 
