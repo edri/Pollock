f3bb7c4e25b71e468ffa95ad98f6104f9744d27e
D34D10CK <leonard.berney@epfl.ch>
2015-10-22 13:15:45 +0200

    add pollSchema

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,35 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', (callback) => {
-	console.log("asdfasf");
+mongoose.connect('mongodb://localhost/test', (error) => {
+	if(error) {
+		console.log(error);
+	} else {
+		console.log("Connected to database");
+	}
 });
+
+var Schema = mongoose.Schema;
+
+var pollsSchema = new Schema({
+	title: String,
+	creationDate: Date,
+	state: String,
+	questions: [{
+		title: String,
+		type: String,
+		choices: [{
+			key: String,
+			text: String,
+			participations: [{
+				participant: String,
+				submissionDate: Date
+			}]
+		}]
+	}],
+});
+
+var polls = mongoose.model("polls", pollsSchema);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
