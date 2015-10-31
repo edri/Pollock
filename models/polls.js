ff4671bb46cc79f5a450a786ea16bdeb9c43043f
D34D10CK <leonard.berney@epfl.ch>
2015-10-31 19:12:59 +0100

    modify schemas and add validation

--- models/polls.js
+++ models/polls.js
@@ -3,22 +3,76 @@ var Schema = mongoose.Schema;
 
 // Create polls' schema.
 var pollsSchema = new Schema({
-	title: String,
-	creationDate: Date,
-	state: String,
+	title: {
+		type: String, 
+		required: true
+	}
+	creationDate: {
+		type Date, 
+		required: true
+	},
+	state: {
+		type: String,
+		required: true
+	},
 	questions: [{
-		title: String,
-		type: {type: String},
+		title: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		},
 		choices: [{
-			key: String,
-			text: String,
-			participations: [{
-				participant: String,
-				submissionDate: Date
-			}]
+			key: {
+				type: String,
+				required: true
+			}
+			text: {
+				type: String,
+				required: true
+			}
 		}]
+	}],
+	participations: [{
+		id: Schema.ObjectId,
+		ref: participations,
+		required: true
+	}]
+});
+
+pollsSchema.path("questions").schema.path("choices").validate(
+	function(questions) {
+		if(!features) {
+			return false;
+		} else if(features.length < 2) {
+			return false;
+		}
+		return true;
+}, "polls need to have at least two questions");
+
+var participationsSchema = new Schema({
+	participant: {
+		type: String,
+		required: true
+	},
+	submissionDate: {
+		type: date,
+		required: true
+	},
+	poll: {
+		type: Schema.ObjectId,
+		ref: polls,
+		required: true
+	answers: [{
+		choice: {
+			type: String,
+			required: true
+		}
 	}]
 });
 
 // Get the polls' model.
 module.exports = mongoose.model("polls", pollsSchema);
+module.exports = mongoose.model("participations", participationsSchema);
