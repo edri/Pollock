b5d7a51361a263d4354e829f87ce670ee0fdd95c
BafS <fabacrans@hotmail.com>
2015-11-01 19:59:59 +0100

    add dev example

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,9 @@ function getTheFirstDayOfTheCurrentWeek() {
 /*
 * Connect to Pollock's mongoose database, and get the polls' model when done.
 */
+// process.env.MONGOLAB_URI = "mongodb://localhost"; // Dev path
 mongoose.connect(process.env.MONGOLAB_URI + '/pollock', function(error) {
+// mongoose.connect(process.env.MONGOLAB_URI + '/pollock', function(error) {
 	if(error) {
 		console.log(error);
 	} else {
