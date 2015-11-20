f4797ea79824af02044145145db956449bec3757
BafS <fabacrans@hotmail.com>
2015-11-20 14:49:26 +0100

    Move hello to app

---
+++ app/bootstrap.ts
@@ -0,0 +1,10 @@
+import {bootstrap} from 'angular2/angular2';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+// import {AppCmp} from './components/app/app';
+
+console.log("> Bootstrap");
+
+// bootstrap(AppCmp, [
+  // ROUTER_PROVIDERS
+// ]);
+//
