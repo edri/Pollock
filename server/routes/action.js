6af5c10f4291dc856c4106e7947440a0410e1bf3
edri <mig.santamar@gmail.com>
2015-11-21 16:13:19 +0100

    Add 'create/access a poll' views.

--- server/routes/action.js
+++ server/routes/action.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('action', {});
+    res.render('action', { title: 'Polls\' actions' });
 });
+
+router.get('/create', function(req, res, next) {
+    res.render('actionCreate', { title: 'Create a poll' });
+});
+
+router.get('/access', function(req, res, next) {
+    res.render('actionAccess', { title: 'Access a poll' });
+});
+
+
 
 module.exports = router;
