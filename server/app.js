f8d4df6be09f41e39becc0b41993de26517d403a
BafS <fabacrans@hotmail.com>
2016-01-07 23:47:22 +0100

    Add eslintrc, clean javascript

--- server/app.js
+++ server/app.js
@@ -1,69 +1,69 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var apiRoutes = require('./routes/api');
-var users = require('./routes/users');
-var components = require('./routes/components');
-
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 'use strict'
+
+let express = require('express')
+let path = require('path')
+// let favicon = require('serve-favicon')
+let logger = require('morgan')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+
+let routes = require('./routes/index')
+let apiRoutes = require('./routes/api')
+let users = require('./routes/users')
+let components = require('./routes/components')
+
+let app = express()
+
 let socketio = require('socket.io')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(require('less-middleware')(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/users', users);
-app.use('/api', apiRoutes);
-app.use('/', routes);
-app.use('/components', components);
+app.use('/users', users)
+app.use('/api', apiRoutes)
+app.use('/', routes)
+app.use('/components', components)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 // Start the server on port 3000.
 let server = app.listen(3000, () => {
@@ -84,4 +84,4 @@ io.on('connection', socket => {
 	})
 })
 
-module.exports = app;
+module.exports = app
