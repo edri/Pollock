dd700bf7e9c3f26911cfc4ec83eb78b3d69f9687
edri <mig.santamar@gmail.com>
2016-01-15 22:26:30 +0100

    Add participations data in stats page.

--- client/components/stats/stats.ts
+++ client/components/stats/stats.ts
@@ -3,6 +3,7 @@ import { ROUTER_DIRECTIVES, RouteParams } from 'angular2/router';
 // Even if your IDE marks this as wrong it is not ; this path is dynamically
 // created by socket.io.
 import io from '/socket.io/socket.io.js';
+
 declare var Chart:any; // Magic
 
 @Component({
@@ -38,7 +39,30 @@ export class Stats {
 	// Get router's ID parameter.
 	constructor(params: RouteParams) {
 		this.id = params.get('id');
-		this.ctx = document.getElementById("chartParticipations");
 		this.getData();
+
+		// TODO: doesn't work yet.
+		/*this.ctx = document.getElementById("chartParticipations");
+
+		let data = [
+			{
+			    value: 25,
+			    label: 'Java',
+			    color: '#811BD6'
+			},
+			{
+			    value: 10,
+			    label: 'Scala',
+			    color: '#9CBABA'
+			}
+		];
+
+		let myPieChart = new Chart(this.ctx, {
+			type: 'pie',
+			data: data,
+			options: {
+				responsiveAnimationDuration: 200
+			}
+		});*/
 	}
 }
