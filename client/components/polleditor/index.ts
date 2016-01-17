15427d54698460293205d545006e055c641172f7
BafS <fabacrans@hotmail.com>
2016-01-17 15:41:08 +0100

    Add NgForm

--- client/components/polleditor/index.ts
+++ client/components/polleditor/index.ts
@@ -1,4 +1,5 @@
 import { Component, View } from 'angular2/core';
+import { NgForm } from 'angular2/common';
 import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 export class Poll {
@@ -12,6 +13,11 @@ export class Poll {
 @View({
 	directives: [ROUTER_DIRECTIVES],
 	templateUrl: 'components/editor'
+	// template: `
+// <input type="text" class="form-control" required
+          // [(ngModel)]="poll.title"
+             // >
+	// `
 })
 export class PollEditor {
 	public poll: Poll = {
